0d1577329957ec090f6e0b17f870ef5023b4941f
Gerrit Ahlers <Gerrit_Ahlers@mckinsey.com>
2021-03-11 19:19:13 +0100

    Main.js 20210311 1915

---
+++ main.js
@@ -0,0 +1,39 @@
+// Array with Zodiacs
+
+const zodiacSignsArr = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricornus",
+    "Aquarius",
+    "Pisces"
+]
+
+// Array with districts in Berlin
+
+const berlinDistrictsArr = [
+    "Friedrichshain",
+    "Kreuzberg",
+    "Mitte",
+    "Prenzlauer Berg",
+]
+
+// Array with oracle forecast
+
+const oracleForecastArr = [
+    "perfect",
+    "good",
+    "ok",
+    "bad",
+    "terrible"
+]
+
+console.log(zodiacSignsArr.length)
+console.log(berlinDistrictsArr.length)
+console.log(zodiacSignsArr.length)
