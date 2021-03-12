9110bc7882e2db8a2907a4e9009041cdaca48750
Gerrit Ahlers <Gerrit_Ahlers@mckinsey.com>
2021-03-12 11:41:24 +0100

    Mixed Message 20210312 1145

--- main.js
+++ main.js
@@ -1,39 +1,17 @@
-// Array with Zodiacs
+// Mixed message object
 
-const zodiacSignsArr = [
-    "Aries",
-    "Taurus",
-    "Gemini",
-    "Cancer",
-    "Leo",
-    "Virgo",
-    "Libra",
-    "Scorpio",
-    "Sagittarius",
-    "Capricornus",
-    "Aquarius",
-    "Pisces"
-]
+const mixedMessage = {
+    zodiac: ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricornus", "Aquarius", "Pisces"],
+    district: ["Kreutzberg", "Friedrichshain", "Mitte", "Prenzlauer Berg"],
+    forecast: ["perfect", "good", "bad", "terrible"],
+    returnMixedMessage () {
+        let randomZodiac = Math.floor(Math.random() * (this.zodiac.length));
+        let randomDistrict = Math.floor(Math.random() * (this.district.length));
+        let randomForecast = Math.floor(Math.random() * (this.forecast.length));
+        console.log(`Every ${this.zodiac[randomZodiac]} in ${this.district[randomDistrict]} will have a ${this.forecast[randomForecast]} day tomorrow!`);
+    }
+}
 
-// Array with districts in Berlin
-
-const berlinDistrictsArr = [
-    "Friedrichshain",
-    "Kreuzberg",
-    "Mitte",
-    "Prenzlauer Berg",
-]
-
-// Array with oracle forecast
-
-const oracleForecastArr = [
-    "perfect",
-    "good",
-    "ok",
-    "bad",
-    "terrible"
-]
-
-console.log(zodiacSignsArr.length)
-console.log(berlinDistrictsArr.length)
-console.log(zodiacSignsArr.length)
+for (i = 0; i < 5; i++) {
+    mixedMessage.returnMixedMessage()
+}
